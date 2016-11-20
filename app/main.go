e409b53adb193b8c7611c3ff6068664804406c5c
AntSe <aseignard+git@gmail.com>
2016-11-20 19:22:04 +0100

    add db statistics to db endpoint

--- app/main.go
+++ app/main.go
@@ -20,16 +20,19 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
 	router.GET("/dbping", func(c *gin.Context) {
 		db, err := sql.Open("postgres", "host=postgres port=5432 user=neo_user dbname=neo_db")
 		if err != nil {
 			log.Fatal(err)
 		}
 		err = db.Ping()
+		dbStats := db.Stats()
 		if err != nil {
 			c.String(http.StatusOK, "hotstName : %s\nDbStatus : %s", os.Getenv("HOSTNAME"), err)
 		}
-		c.String(http.StatusOK, "\nhotstName : %s\nDbStatus :db good", os.Getenv("HOSTNAME"))
+		c.String(http.StatusOK, "\nhotstName : %s\nDbStatus :db good\ndb stats : %d", os.Getenv("HOSTNAME"), dbStats.OpenConnections)
 	})
+
 	router.Run(":" + port)
 }
