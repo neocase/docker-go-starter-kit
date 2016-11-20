58444bdeaa289c2e277e9a4add59e0d998ecc385
AntSe <aseignard+github@gmail.com>
2016-11-20 21:37:11 +0100

    remove table creation in initdb script

--- Dockerfiles/setup-database.sh
+++ Dockerfiles/setup-database.sh
@@ -5,9 +5,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER $POSTGRES_USER;
     CREATE DATABASE $POSTGRES_NAME;
     GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_NAME TO $POSTGRES_USER;
-
-    CREATE TABLE titi (
-     did    integer PRIMARY KEY DEFAULT nextval('serial'),
-     name   varchar(40) NOT NULL CHECK (name <> '')
-    );
 EOSQL
