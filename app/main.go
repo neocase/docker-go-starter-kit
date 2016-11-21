7552312a68de62def6a18ba6b0950f55b524caee
Samir Bouaked <samir.bouaked@gmail.com>
2016-11-21 09:56:00 +0100

    update /db to display errors

--- app/main.go
+++ app/main.go
@@ -21,18 +21,22 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	router.GET("/dbping", func(c *gin.Context) {
+	router.GET("/db", func(c *gin.Context) {
 		db, err := sql.Open("postgres", "host=postgres port=5432 user=neo_user dbname=neo_db")
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer db.Close()
 		err = db.Ping()
 		dbStats := db.Stats()
+		log.Println(dbStats)
+		log.Println("OpenConnections")
+		log.Println(dbStats.OpenConnections)
 		if err != nil {
 			c.String(http.StatusOK, "hotstName : %s\nDbStatus : %s", os.Getenv("HOSTNAME"), err)
+		} else {
+			c.String(http.StatusOK, "\nhotstName : %s\nDbStatus :db good\ndb stats : %d", os.Getenv("HOSTNAME"), dbStats.OpenConnections)
 		}
-		c.String(http.StatusOK, "\nhotstName : %s\nDbStatus :db good\ndb stats : %d", os.Getenv("HOSTNAME"), dbStats.OpenConnections)
+		defer db.Close()
 	})
 
 	router.Run(":" + port)
