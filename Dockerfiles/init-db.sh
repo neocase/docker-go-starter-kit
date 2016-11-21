0af7f0904fe7f3fe442b4b2958b87cf562826864
Samir Bouaked <samir.bouaked@gmail.com>
2016-11-21 17:42:41 +0100

    add again the sh script for test

--- Dockerfiles/init-db.sh
+++ Dockerfiles/init-db.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "INSERT HERE YOUR SCRIPT"
+psql -U postgres --command "CREATE USER docker WITH PASSWORD 'docker';"
+psql -U postgres --command "CREATE DATABASE docker;"
+psql -U postgres --command "GRANT ALL PRIVILEGES ON DATABASE docker TO docker;"
--- Dockerfiles/postgres.dockerfile
+++ Dockerfiles/postgres.dockerfile
@@ -2,10 +2,6 @@ FROM library/postgres
 
 MAINTAINER Samir Bouaked "sbouaked@neocasesoftware.com"
 
-ENV POSTGRES_USER = docker
-ENV POSTGRES_PASSWORD = docker
-ENV POSTGRES_DB = docker
-
 ADD Dockerfiles/init-db.sh /docker-entrypoint-initdb.d/
 
 EXPOSE 5432
